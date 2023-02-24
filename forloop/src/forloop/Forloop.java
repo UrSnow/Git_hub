ec5fe9314414bc131b2b7c0ebc9c3a41f201fccd
UrSnow <119578665+UrSnow@users.noreply.github.com>
2023-02-24 15:34:33 +0200

    Add files via upload

---
+++ forloop/src/forloop/Forloop.java
@@ -0,0 +1,28 @@
+
+package forloop;
+
+public class Forloop {
+
+
+    public static void main(String[] args) {
+      
+   int a; //declaration
+        a = 2;//assignment
+       int b=3;
+       
+       boolean ans =(double) a!=b;
+       
+        {
+         
+        System.out.print(ans);
+        }
+        {
+     for( int i=0;  i <=10; i++)
+       
+     {
+     
+     System.out.println(i);
+     }
+    }
+    }
+}
